6f47d050a75e86ca047252f64ffb8b25c95511ad
Zheyu Shen <arsdragonfly@gmail.com>
2023-08-06 02:00:01 +0800

    server side renet boilerplate

--- ultralogi-server/src/main.rs
+++ ultralogi-server/src/main.rs
@@ -1,3 +1,77 @@
+use std::{net::UdpSocket, time::SystemTime};
+
+use bevy::prelude::*;
+use bevy_renet::{
+    renet::{
+        transport::{NetcodeServerTransport, ServerAuthentication, ServerConfig},
+        ConnectionConfig, DefaultChannel, RenetServer, ServerEvent,
+    },
+    transport::NetcodeServerPlugin,
+    RenetServerPlugin,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_plugins(MinimalPlugins);
+    app.add_plugins(RenetServerPlugin);
+    let server = RenetServer::new(ConnectionConfig::default());
+    app.insert_resource(server);
+    app.add_plugins(NetcodeServerPlugin);
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
+    const MAX_CLIENTS: usize = 64;
+    const GAME_PROTOCOL_ID: u64 = 0;
+    let server_config = ServerConfig {
+        max_clients: MAX_CLIENTS,
+        protocol_id: GAME_PROTOCOL_ID,
+        public_addr: server_addr,
+        authentication: ServerAuthentication::Unsecure,
+    };
+    let current_time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+    let transport = NetcodeServerTransport::new(current_time, server_config, socket).unwrap();
+    app.insert_resource(transport);
+    app.add_system(send_message_system);
+    app.add_system(receive_message_system);
+    app.add_system(handle_events_system);
+    app.run();
+}
+
+fn send_message_system(mut server: ResMut<RenetServer>) {
+    let channel_id = 0;
+    // Send a text message for all clients
+    // The enum DefaultChannel describe the channels used by the default configuration
+    server.broadcast_message(
+        DefaultChannel::ReliableOrdered,
+        "server message".as_bytes().to_vec(),
+    );
+}
+
+fn receive_message_system(mut server: ResMut<RenetServer>) {
+    // Send a text message for all clients
+    for client_id in server.clients_id().into_iter() {
+        while let Some(message) = server.receive_message(client_id, DefaultChannel::ReliableOrdered)
+        {
+            // Handle received message
+        }
+    }
+}
+
+fn handle_events_system(
+    mut server: ResMut<RenetServer>,
+    mut server_events: EventReader<ServerEvent>,
+) {
+    while let Some(event) = server.get_event() {
+        for event in server_events.iter() {
+            match event {
+                ServerEvent::ClientConnected { client_id } => {
+                    println!("Client {client_id} connected");
+                }
+                ServerEvent::ClientDisconnected { client_id, reason } => {
+                    println!("Client {client_id} disconnected: {reason}");
+                }
+            }
+        }
+    }
 }
